ef3cd7f256041485529fa6bfb7b9690ebf44dfe6
Bleuzen Benjamin <bleuzen1@mmi-dev.unilim.fr>
2026-04-22 13:43:18 +0200

    update model.php

--- server/model.php
+++ server/model.php
@@ -14,9 +14,9 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "morap01");
-define("DBLOGIN", "morap01");
-define("DBPWD", "morap01");
+define("DBNAME", "bleuzen1");
+define("DBLOGIN", "bleuzen1");
+define("DBPWD", "bleuzen1");
 
 
 function getAllMovies(){
