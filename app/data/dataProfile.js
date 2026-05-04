b33319a2fc16cd1ffaa69e204b0ca4a5af7c96e1
Bleuzen Benjamin <bleuzen1@mmi-dev.unilim.fr>
2026-05-04 15:53:43 +0200

    hebergement

--- app/data/dataProfile.js
+++ app/data/dataProfile.js
@@ -1,11 +1,11 @@
 // URL où se trouve le répertoire "server" sur mmi.unilim.fr
-let HOST_URL = "https://mmi.unilim.fr/~bleuzen1/SAE2.03-Bleuzen";//"http://mmi.unilim.fr/~????"; // CHANGE THIS TO MATCH YOUR CONFIG
+let HOST_URL = "../..";//"http://mmi.unilim.fr/~????"; // CHANGE THIS TO MATCH YOUR CONFIG
 
 let DataProfile = {};
 
 DataProfile.requestProfile = async function(){
 
-    let answer = await fetch(HOST_URL + "/server/script.php?todo=readProfile");
+    let answer = await fetch(HOST_URL + "../server/script.php?todo=readProfile");
     let data = await answer.json();
     return data;
 }
